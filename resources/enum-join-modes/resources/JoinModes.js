6f24464c290f9c9da5910091de91bf6a711f84c4
alkashi <hoyeungw@outlook.com>
2020-03-16 15:30:14 +0800

    v0.1.0

--- resources/enum-join-modes/resources/JoinModes.js
+++ resources/enum-join-modes/resources/JoinModes.js
@@ -3,7 +3,7 @@ export const UNION = 0
 export const LEFT = 1
 export const RIGHT = 2
 
-export const JoinTypes = {
+export const JoinModes = {
   intersect: INTERSECT,
   union: UNION,
   left: LEFT,
