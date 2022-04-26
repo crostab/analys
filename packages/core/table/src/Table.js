8b3ed9979a0b4630b9910420a5a4f5e25618fb2a
hoyeungw <hoyeungw@outlook.com>
2022-04-26 11:33:18 +0800

    minor fix

--- packages/core/table/src/Table.js
+++ packages/core/table/src/Table.js
@@ -339,7 +339,7 @@ export class Table {
    * @param {Object|*[]|string|number} [options.field]
    * @param {Object<string|number,function(*?):boolean>} [options.filter]
    * @param {function(...*):number} [options.formula] - formula is valid only when cell is CubeCell array.
-   * @returns {CrosTab}
+   * @returns {Crostab|CrostabObject}
    */
   crosTab(options = {}) {
     const table = slice(this)
