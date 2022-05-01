039f66548360da01781fadb0e44de8cbb2329fcc
hoyeungw <hoyeungw@outlook.com>
2022-05-01 20:06:26 +0800

    add conversion among crostab, table and matrix

---
+++ packages/core/convert/src/specialization.js
@@ -0,0 +1,18 @@
+import { acquire } from '@vect/vector-merge'
+import { zipper }  from '@vect/vector-zipper'
+
+
+export const crostabToTable = (crostab, title) => {
+  const head = acquire([ title ?? crostab.title ?? '' ], crostab.head)
+  const rows = zipper(crostab.side, crostab.rows, (x, row) => acquire([ x ], row))
+  return { head, rows }
+}
+
+export const tableToMatrix = table => {
+  const { head, rows } = table
+  return acquire([ head ], rows)
+}
+
+export const crostabToMatrix = (crostab, title) => {
+  return crostabToTable(crostab, title) |> tableToMatrix
+}
