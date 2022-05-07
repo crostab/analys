d4c82d190d86a6f9c183c19c99b0b587b6ae7305
hoyeungw <hoyeungw@outlook.com>
2022-05-07 14:24:01 +0800

    expand functionality to crostabToNested

--- packages/core/convert/src/crostabToNested.js
+++ packages/core/convert/src/crostabToNested.js
@@ -1,13 +1,9 @@
-import { filterIndexed, simpleIndexed } from '@analys/crostab-indexed'
-import { FUN }                          from '@typen/enum-data-types'
+import { indexed }    from '@analys/crostab-indexed'
+import { updateCell } from '@vect/nested'
 
-export const crostabToNested = (crostab, filter) => {
-  // const by = conf?.by, to = conf?.to ?? conf
+export const crostabToNested = (crostab, by, to) => {
   const o = {}
-  if (typeof filter === FUN) {
-    for (const [x, y, v] of filterIndexed(crostab, filter)) (o[x] ?? (o[x] = {}))[y] = v
-  } else {
-    for (const [x, y, v] of simpleIndexed(crostab)) (o[x] ?? (o[x] = {}))[y] = v
-  }
+  for (const [ x, y, v ] of indexed(crostab, by, to)) updateCell.call(o, x, y, v)
   return o
 }
+
